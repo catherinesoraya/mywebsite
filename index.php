b4bc51da68ecba0538228c9ebaafcd27c38d689e
Catherine Soraya Yazdanpour <Soraya@Catherines-MacBook-Pro-2.local>
2017-02-20 08:52:46 -0700

    changes

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php include("header.php");
+<div id="content">
+                <h2>Home</h2>
+                <h4>Recent News</h4>
+        <span id="recentText1">  <h5>Coming Soon: A Podcast! 01/01/2017</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ex eorum enim scriptis et institutis cum omnis doctrina liberalis, omnis historia. Hoc non est positum in nostra actione. Quis est, qui non oderit libidinosam, protervam adolescentiam? At iam decimum annum in spelunca iacet. Hic ambiguo ludimur. Duo Reges: constructio interrete.</p>
+
+
+        <span id="recentText">  <h5>New Pieces from Baltimore 12/12/2016</h5>
+        <p>     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ex eorum enim scriptis et institutis cum omnis doctrina liberalis, omnis historia. Hoc non est positum in nostra actione. Quis est, qui non oderit libidinosam, protervam adolescentiam? At iam decimum annum in spelunca iacet. Hic ambiguo ludimur. Duo Reges: constructio interrete.</p>
+        </span>
+        <span id="recentPics">
+          <img id="recentPic" src="pics/recent1.jpg"/>
+                <img id="recentPic1" src="pics/recent2.jpg"/>
+        </span>
+
+
 	include("footer.php");
  ?>
 
