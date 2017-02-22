ce489e4fc1a1e11cb96c5b9b2882d9bf7770348c
Catherine Soraya Yazdanpour <Soraya@CatherinesMBP2.boisestate.edu>
2017-02-22 15:10:53 -0700

    changes

--- forum.php
+++ forum.php
@@ -4,8 +4,9 @@
 ?>
 	<div id="content">
 		<h2>Forum</h2>
-
-
+		<h3>no threads yet<h3>
+		<button type="button"><a href="newThread.php">create a thread</a></button>
+		
 
 	</div>	
 <?php
---
+++ newThread.php
@@ -0,0 +1,12 @@
+<?php
+
+	include("header.php");
+	
+?>
+
+	<div id="content">
+		title: <input type="text" id="title" name="title">
+		<br>
+		body: <input type="text" id="threadBody" name="threadBody">
+
+	</div>
