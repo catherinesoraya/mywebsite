ebaff9bf605e7766229db53a03387d8a9e053a92
Catherine Soraya Yazdanpour <Soraya@Catherines-MacBook-Pro-2.local>
2017-02-21 22:37:50 -0700

    changes

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
 	<div id="content">
 		<h2>About the Artist</h2>
 
-		<img id="manu1" src="pics/manu1.jpg"/>
+		<img id="manu1" src="pics/manu1.JPG"/>
 	
 		<span id="aboutText"><h3 align="center"> Manucher Lenjani Yazdanpour</h3>
 		<h4 align="center">1939 - 1974</h4>
