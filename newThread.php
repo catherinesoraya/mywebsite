b76b1d8c448770634d70d79991ac25d35fcec241
Catherine Soraya Yazdanpour <Soraya@CatherinesMBP2.boisestate.edu>
2017-02-22 15:29:02 -0700

    changes

--- newThread.php
+++ newThread.php
@@ -7,7 +7,7 @@
 	<div id="content">
 		title: <input type="text" id="threadTitle" name="threadtitle">
 		<br>
-		body: <input type="text" id="threadBody" name="threadbody">
+		body: <textarea id="threadBody" rows="20" cols="100"></textarea>
 
 	</div>
 
