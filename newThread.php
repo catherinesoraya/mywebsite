9384765e625e037e52c2ea89a07810b06f7d3e21
Catherine Soraya Yazdanpour <Soraya@CatherinesMBP2.boisestate.edu>
2017-02-22 19:05:10 -0700

    changes

--- newThread.php
+++ newThread.php
@@ -8,7 +8,7 @@
 		title: <input type="text" id="threadTitle" name="threadtitle">
 		<br>
 		body: <input type="text" id="threadBody" name="threadBody">
-
+		<button type="button" id="post">post</button>
 	</div>
 
 <?php
