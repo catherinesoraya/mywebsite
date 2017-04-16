813146449ecbad87463018bd0380b260f91fa598
Catherine Soraya Yazdanpour <Soraya@Catherines-MacBook-Pro-2.local>
2017-04-16 14:45:12 -0600

    working

--- gallery.php
+++ gallery.php
@@ -4,27 +4,27 @@
 
         <div id= "content">
                 <h2>Gallery</h2>
-	</div>
-	<div id ="slider">
 	<div class ="gallery">		
 		
 		<img id="gallery" src="pics/woman.JPG"/>
-		<img id="gallery" src="pics/oldman.JPG"/>
 	
-		<img id="gallery" src="pics/out2.JPG"/>
-		<img id="gallery" src="pics/out1.JPG"/>
-
-		<img id="gallery" src="pics/ou3.JPG"/>
+		<img id="gallery" src="pics/blueMosque.png"/>
+		<img id="gallery" src="pics/oldman.JPG"/>
+		<img id="gallery" src="pics/whitemosque.png"/>
+		<img id="gallery" src="pics/readingman.png"/>
+		<img id="gallery" src="pics/pipe.png"/>
+		<img id="gallery" src="pics/bluemosquebazzar.png"/>
+		<img id="gallery" src="pics/motherchild.png"/>
+		 <img id="gallery" src="pics/soldier.png"/>
+		<img id="gallery" src="pics/village.png"/>
 	</div>
 	</div>
 	<script>
-	
 	$('.gallery').slick({
- 		 infinite: true,
- 		 slidesToShow: 3,
- 		 slidesToScroll: 6
-
-	});	
+	infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+});
 	</script>    
 
 
